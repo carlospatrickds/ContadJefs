696fd3dfeae5deb39c67bade44ffe0d31019a00b
carpasi <carlos.patrick@hotmail.com>
2026-04-02 11:13:12 -0300

    Update pss_progressivo.py

--- pss_progressivo.py
+++ pss_progressivo.py
@@ -384,7 +384,7 @@ with st.sidebar:
     
     # Define a cor RGB baseada na seleção
     if tema_selecionado == "Salmão Claro":
-        cor_tema_rgb = (255, 224, 214)
+        cor_tema_rgb = (252, 179, 170)
     else:
         cor_tema_rgb = (235, 235, 235)
 
